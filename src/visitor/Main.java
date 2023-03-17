11993d285e084e0da76db3c1eaed8f3b471eb2da
zazazanoza <48163159+zazazanoza@users.noreply.github.com>
2023-03-17 22:37:50 +0900

    fix Visitor Pattern

--- src/visitor/Main.java
+++ src/visitor/Main.java
@@ -3,9 +3,11 @@ package visitor;
 public class Main {
 
     public static void main(String[] args) {
-        Maria maria = new Maria();
-        maria.visit(new InnA());
-        maria.visit(new InnB());
+        InnA innA = new InnA();
+        InnB innB = new InnB();
+        innA.accept(new Maria());
+        innB.accept(new Maria());
+
     }
 
 }
